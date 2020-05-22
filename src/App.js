f114996657a7e882fd8b8009681494b29228b7c7
Josh Wood <gdjoshwood@gmail.com>
2020-05-21 20:35:20 -0700

    Properly functioning transition states

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import './App.css';
 import {FSA} from './app-constants';
 
 function App() {
+  const [currentState, setCurrentState] = useState(FSA.STATES[FSA.INITIAL_STATE]);
+  const onInputAction = useCallback(ev => {
+    const newState = FSA.TRANSITIONS.find(transition => (transition.from === currentState 
+      && transition.input === ev.target.value)).to;
+    setCurrentState(newState)
+  }, [currentState]);
+ 
   return (
     <div className="App">
       <h1>Finite State Automata</h1>
       <main>
         <h2>Change State <small>by pressing a button</small></h2>
-        <button type="button">0</button>
-        <button type="button">1</button>
-        <div>{fun}</div>
+        <button type="button" value="0" onClick={onInputAction}>0</button>
+        <button type="button" value="1" onClick={onInputAction}>1</button>
+        <div>{currentState}</div>
       </main>
     </div>
   );
--- src/app-constants.js
+++ src/app-constants.js
@@ -1,29 +1,29 @@
 const STATES = ['S0', 'S1', 'S2'];
-const INITIAL_STATE = `${STATES[0]}`;
-const INPUTS = [0, 1];
+const INITIAL_STATE = 0;
+const INPUTS = ['0', '1'];
 const FINAL_STATES = [...STATES];
 const TRANSITIONS = [{
-  input: 0,
+  input: INPUTS[0],
   from: STATES[0],
   to: STATES[0]
 }, {
-  input: 1,
+  input: INPUTS[1],
   from: STATES[0],
   to: STATES[1]
 }, {
-  input: 0,
+  input: INPUTS[0],
   from: STATES[1],
   to: STATES[2]
 }, {
-  input: 1,
+  input: INPUTS[1],
   from: STATES[1],
   to: STATES[0]
 }, {
-  input: 0,
+  input: INPUTS[0],
   from: STATES[2],
   to: STATES[0]
 }, {
-  input: 1,
+  input: INPUTS[1],
   from: STATES[2],
   to: STATES[2]
 }];
