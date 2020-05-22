0750107e8000450c6e6f1331f5f699f5bc9d9861
Josh Wood <gdjoshwood@gmail.com>
2020-05-21 20:14:42 -0700

    Basic layout structure start

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
+import {FSA} from './app-constants';
 
 function App() {
   return (
     <div className="App">
-
+      <h1>Finite State Automata</h1>
+      <main>
+        <h2>Change State <small>by pressing a button</small></h2>
+        <button type="button">0</button>
+        <button type="button">1</button>
+        <div>{fun}</div>
+      </main>
     </div>
   );
 }
